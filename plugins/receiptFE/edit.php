49a0b19cc266f827331f65f440c14666d54f4628
Maicol <maicolbattistini@live.it>
2019-05-24 16:38:36 +0200

    Revert "Fix e miglioramenti after 2.4.9 (2.4.9A) (#18)"
    This reverts commit 24c7d309efa248c1ba9841495bc94196a5053a3a.

--- plugins/receiptFE/edit.php
+++ plugins/receiptFE/edit.php
@@ -12,9 +12,9 @@ if (!Interaction::isEnabled()) {
 }
 
 echo '
-<p>'.tr('Le ricevute delle Fatture Elettroniche permettono di individuare se una determinata fattura tramessa è stata accettata dal Sistema Di Interscambio').'.</p>
+<p>'.tr('Le ricevute delle Fatture Elettroniche permettono di individuare se una determinata fattura rilasciata è stata accettata dal Sistema Di Interscambio e dal cliente relativo').'.</p>
 
-<p>'.tr("Tramite il pulsante _BTN_ è possibile procedere al recupero delle ricevute, aggiornando automaticamente lo stato delle relative fatture e allegandole ad esse", [
+<p>'.tr("Tramite il pulsante _BTN_ è possibile procedere all controllo automatico di queste ricevute, che aggiorneranno di conseguenza lo $requesto dei documenti relativi e verranno allegate ad essi", [
     '_BTN_' => '<b>Ricerca</b>',
 ]).'.</p>
 <br>';
